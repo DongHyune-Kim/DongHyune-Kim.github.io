9d475ce52efea91f17ff08b362d9b1513b4da040
Donghyune KIM <168303804+DongHyune-Kim@users.noreply.github.com>
2024-04-28 19:11:13 +0900

    Update 2024-04-28-Week2 Corejs.md.js

--- 2024-04-28-Week2 Corejs.md.js
+++ 2024-04-28-Week2 Corejs.md.js
@@ -1,41 +1,42 @@
-// Example 1-2
-
+# Example 1-2
+```js
 var a;
-
-// Example 1-2
-
+```
+# Example 1-2
+```js
 var a; 
 a = 'abc'; 
-
+```
 var a = 'abc';
 
-// Example 1-3
-
+# Example 1-3
+```js
 var a = 'abc';
 a = a + 'def';
 
 var b = 5;
 var c = 5;
 b = 7;
-
-// Example 1-4
-
+```
+# Example 1-4
+```js
 var obj1 = {
   a: 1,
   b: 'bbb',
 };
-
-// Example 1-5
-
+```
+# Example 1-5
+```js
 var obj1 = {
   a: 1,
   b: 'bbb',
 };
 obj1.a = 2;
-
-// Example 1-6
-
+```
+# Example 1-6
+```js
 var obj = {
   x: 3,
   arr: [3, 4, 5],
 };
+```
