8b39a58c416709a8483c6e0aa083902b93589b9e
Donghyune KIM <168303804+DongHyune-Kim@users.noreply.github.com>
2024-04-29 02:54:44 +0900

    Add files via upload

---
+++ _posts/2024-04-28-Week6-3 Corejs.js
@@ -0,0 +1,82 @@
+// Example 3-1
+console.log(this); // { alert: f(), atob: f(), blur: f(), btoa: f(), ... }
+console.log(window); // { alert: f(), atob: f(), blur: f(), btoa: f(), ... }
+console.log(this === window); // true
+
+// Example 3-2
+console.log(this); // { process: { title: 'node', version: 'v10.13.0',... } }
+console.log(global); // { process: { title: 'node', version: 'v10.13.0',... } }
+console.log(this === global); // true
+
+// Example 3-3
+var a = 1;
+console.log(a); // 1
+console.log(window.a); // 1
+console.log(this.a); // 1
+
+// Example 3-4
+var a = 1;
+window.b = 2;
+console.log(a, window.a, this.a); // 1 1 1
+console.log(b, window.b, this.b); // 2 2 2
+
+window.a = 3;
+b = 4;
+console.log(a, window.a, this.a); // 3 3 3
+console.log(b, window.b, this.b); // 4 4 4
+
+// Example 3-5
+var a = 1;
+delete window.a; // false
+console.log(a, window.a, this.a); // 1 1 1
+
+var b = 2;
+delete b; // false
+console.log(b, window.b, this.b); // 2 2 2
+
+window.c = 3;
+delete window.c; // true
+console.log(c, window.c, this.c); // Uncaught ReferenceError: c is not defined
+
+window.d = 4;
+delete d; // true
+console.log(d, window.d, this.d); // Uncaught ReferenceError: d is not defined
+
+// Example 3-6
+var func = function(x) {
+  console.log(this, x);
+};
+func(1); // Window { ... } 1
+
+var obj = {
+  method: func,
+};
+obj.method(2); // { method: f } 2
+
+// Example 3-7
+var obj = {
+  method: function(x) {
+    console.log(this, x);
+  },
+};
+obj.method(1); // { method: f } 1
+obj['method'](2); // { method: f } 2
+
+// Example 3-8
+var obj = {
+  methodA: function() {
+    console.log(this);
+  },
+  inner: {
+    methodB: function() {
+      console.log(this);
+    },
+  },
+};
+obj.methodA(); // { methodA: f, inner: {...} }    ( === obj)
+obj['methodA'](); // { methodA: f, inner: {...} } ( === obj)
+
+obj.inner.methodB(); // { methodB: f }            ( === obj.inner)
+obj.inner['methodB'](); // { methodB: f }         ( === obj.inner)
+obj['inner'].methodB(); // { methodB: f }         ( === obj.inner)
+obj['inner']['methodB'](); // { methodB: f }      ( === obj.inner)
