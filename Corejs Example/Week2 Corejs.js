07fcbc004394f6883c9c65182e9a5b5fb25d7b59
Donghyune-Kim <168303804+DongHyune-Kim@users.noreply.github.com>
2024-04-28 17:39:47 +0900

    Week2 Corejs Example.js

---
+++ Corejs Example/Week2 Corejs.js
@@ -0,0 +1,44 @@
+// Example 1-2
+var a;
+
+// Example 1-2
+
+var a; 
+a = 'abc'; 
+
+var a = 'abc';
+
+
+// Example 1-3
+
+var a = 'abc';
+a = a + 'def';
+
+var b = 5;
+var c = 5;
+b = 7;
+
+
+// Example 1-4
+
+var obj1 = {
+  a: 1,
+  b: 'bbb',
+};
+
+
+// Example 1-5
+
+var obj1 = {
+  a: 1,
+  b: 'bbb',
+};
+obj1.a = 2;
+
+
+// Example 1-6
+
+var obj = {
+  x: 3,
+  arr: [3, 4, 5],
+};
